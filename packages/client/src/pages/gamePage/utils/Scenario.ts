2f97c487c4c97c7343db99f6aa0ef3828939f457
Alexander Ershov <rufflet@ya.ru>
2022-11-30 02:13:24 +0300

    feat(client): реализована механика игры

---
+++ packages/client/src/pages/gamePage/utils/Scenario.ts
@@ -0,0 +1,96 @@
+import roadImg from '../assets/scenario/road.jpg';
+import { Tree } from './roadside/Tree';
+import { GameConfig } from '@/pages/gamePage/utils/game.config';
+
+/**
+ * Класс определяющий сценарий игры
+ */
+export class Scenario {
+  roadImage = new Image();
+
+  roadImageWidth = 500;
+
+  roadImageHeight = 600;
+
+  canvas!: HTMLCanvasElement;
+
+  x!: number;
+
+  y!: number;
+
+  y2!: number;
+
+  trees: Tree[] = [];
+
+  /**
+   * Конструктор класса сценария.
+   * @param {HTMLCanvasElement} canvas - Элемент Canvas.
+   */
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.canvas.style.backgroundColor = 'green';
+
+    this.x = canvas.width / 2 - this.roadImageWidth / 2;
+    this.y = 0;
+    this.y2 = -this.roadImageHeight;
+
+    this.roadImage.src = roadImg;
+
+    this.createTrees();
+  }
+
+  /**
+   * Отрисовка дороги (в т.ч. обочины)
+   * @param {CanvasRenderingContext2D} context - Контекст Canvas.
+   */
+  drawRoad(context: CanvasRenderingContext2D) {
+    context.drawImage(this.roadImage, this.x, this.y, this.roadImageWidth, this.roadImageHeight);
+    context.drawImage(this.roadImage, this.x, this.y2, this.roadImageWidth, this.roadImageHeight);
+
+    this.trees.forEach((tree) => {
+      tree.draw(context);
+    });
+  }
+
+  /**
+   * Обновление дороги (в т.ч. обочины)
+   * @param {number} speed - Скорость смещения.
+   */
+  updateRoad(speed: number) {
+    this.y += speed;
+    if (this.y >= this.roadImageHeight) this.y = 0;
+
+    this.y2 += speed;
+    if (this.y2 >= 0) this.y2 = -this.roadImageHeight;
+
+    this.updateTrees(speed);
+  }
+
+  /**
+   * Создание деревьев
+   */
+  createTrees() {
+    const sideBreakPoint = Math.floor((GameConfig.roadside.trees - 1) / 2);
+    let treePositionIndex = 0;
+    let treeSide = 0;
+
+    for (let i = 0; i < GameConfig.roadside.trees; i++) {
+      this.trees[i] = new Tree(treePositionIndex, treeSide);
+
+      if (treePositionIndex >= sideBreakPoint) {
+        treePositionIndex = 0;
+        treeSide = 1;
+      } else treePositionIndex++;
+    }
+  }
+
+  /**
+   * Обновление деревьев
+   * @param {number} speed - Скорость смещения.
+   */
+  updateTrees(speed: number) {
+    this.trees.forEach((tree) => {
+      tree.update(this.canvas.height, speed);
+    });
+  }
+}
---
+++ packages/client/src/pages/gamePage/utils/game.config.ts
@@ -0,0 +1,27 @@
+export const GameConfig = {
+  level: {
+    initialSpeed: 3,
+    amountIncrease: 1,
+  },
+  roadside: {
+    trees: 10,
+    treeDistance: 60,
+  },
+  collision: {
+    nearDistanceX: 20,
+    nearDistanceY: 100,
+  },
+  scenario: {
+    numberOfLanes: 4,
+    lanesSize: 120,
+  },
+  traffic: {
+    minCarSpeed: 0.3,
+    maxCarSpeed: 0.8,
+    cars: 5,
+  },
+  player: {
+    carType: 0,
+    initialY: 390,
+  },
+};
