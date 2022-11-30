a0d012be8ba52e2aff646dc4f6d549bc402b1d86
Valeria Stepanova <shestakovalera200@mail.ru>
2022-11-30 16:04:05 +0300

    feat: add profilePage and styles

--- packages/client/src/pages/userInfoPage/UserInfoPage.tsx
+++ packages/client/src/pages/userInfoPage/UserInfoPage.tsx
@@ -1,10 +1,53 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { Typography, Form, Input, Button } from 'antd';
 import { RoutePath } from '@/utils/router/routeConfig';
+import './UserInfoPage.scss';
+
+const layout = {
+  labelCol: { span: 10 },
+  wrapperCol: { span: 34 },
+};
+const { Title } = Typography;
 
 export const UserInfoPage = () => (
-  <div>
-    userInfoPage
+  <div className="profile">
+    <Form {...layout} name="basic" className="profile__form" layout="vertical">
+      <Title className="profile__form-title">Профиль</Title>
+      <Form.Item className="profile__form-item" label="Почта" name="email">
+        <Input />
+      </Form.Item>
+      <Form.Item className="profile__form-item" label="Логин" name="login">
+        <Input />
+      </Form.Item>
+      <Form.Item className="profile__form-item" label="Имя" name="first_name">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        className="profile__form-item"
+        label="Фамилия"
+        name="second_name">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        className="profile__form-item"
+        label="Имя в игре"
+        name="display_name">
+        <Input />
+      </Form.Item>
+      <Form.Item className="profile__form-item" label="Телефон" name="phone">
+        <Input />
+      </Form.Item>
+      <Form.Item className="profile__form-item">
+        <Button className="profile__button" htmlType="submit" type="primary">
+          Сохранить
+        </Button>
+        <Button className="profile__button" htmlType="button" type="primary">
+          Изменить пароль
+        </Button>
+      </Form.Item>
+    </Form>
     <Link to={RoutePath.main}> Главное меню </Link>
   </div>
 );
