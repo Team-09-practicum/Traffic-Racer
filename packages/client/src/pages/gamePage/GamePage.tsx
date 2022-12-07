86e4707e97be5109254790d29b0592b323b201e3
Alex Ershov <rufflet@ya.ru>
2022-12-07 23:09:21 +0300

    TM-09-62 экран превью игры (#19)
    fix(client): game preview screen

--- packages/client/src/pages/gamePage/GamePage.tsx
+++ packages/client/src/pages/gamePage/GamePage.tsx
@@ -1,28 +1,41 @@
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
-import { appRoutes } from '@/utils/router/appRoutes';
-import { TrafficRacer } from './components/TrafficRacer';
+import React, { useEffect, useRef, useState } from 'react';
+import { Space, Typography } from 'antd';
+import { TrafficRacer } from './components/TrafficRacer/TrafficRacer';
 import { GameStart } from './components/startGame/GameStart';
 import { GameEnd } from './components/gameEnd/GameEnd';
+import { useWindowSize } from '@/pages/gamePage/hooks/useWindowSize';
+import './GamePage.scss';
+
+const { Text, Title } = Typography;
 
 export const GamePage = () => {
+  const { height } = useWindowSize();
+  const [pageTopOffset, setPageTopOffset] = useState(0);
   const [isGameStarted, setGameStarted] = useState(false);
   const [isGameOver, setGameOver] = useState(false);
   const [score, setScore] = useState(0);
   const isFirstStart = !isGameStarted && !isGameOver;
+  const gamePageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (gamePageRef.current) setPageTopOffset(gamePageRef.current.getBoundingClientRect().top);
+  }, []);
 
   return (
-    <div>
-      <Link to={appRoutes.main}> Главное меню </Link>
-
+    <div ref={gamePageRef} className="game-page">
       {isFirstStart && <GameStart />}
-      {isGameStarted && <div>Игра началась</div>}
       {isGameOver && <GameEnd score={score} />}
-      <div>Score: {score}</div>
+      {isGameStarted && (
+        <div className="game-page__score">
+          <Space direction="vertical" align="end">
+            <Text strong>Счёт:</Text>
+            <Title>{score}</Title>
+          </Space>
+        </div>
+      )}
 
       <TrafficRacer
-        height={500}
-        width={800}
+        height={height - pageTopOffset}
         setGameStarted={setGameStarted}
         setGameOver={setGameOver}
         setScore={setScore}
