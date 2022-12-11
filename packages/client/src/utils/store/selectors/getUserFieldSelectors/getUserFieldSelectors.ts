fdf06e9f67d46e76a27feaf61bb7e70f25826b65
Al <Batuten.mail@gmail.com>
2022-12-11 19:38:00 +0400

    fix: selectors (redux)

--- packages/client/src/utils/store/selectors/getUserFieldSelectors/getUserFieldSelectors.ts
+++ packages/client/src/utils/store/selectors/getUserFieldSelectors/getUserFieldSelectors.ts
@@ -1,17 +1,17 @@
-import type { IUser } from '@/typings/IUser';
+import { IStateScheme } from '@/typings/IStateSchema';
 
-export const getUserId = (state: IUser) => state.id;
+export const getUserId = (state: IStateScheme) => state.user.id;
 
-export const getUserEmail = (state: IUser) => state.email;
+export const getUserEmail = (state: IStateScheme) => state.user.email;
 
-export const getUserLogin = (state: IUser) => state.login;
+export const getUserLogin = (state: IStateScheme) => state.user.login;
 
-export const getUserFirstName = (state: IUser) => state.first_name;
+export const getUserFirstName = (state: IStateScheme) => state.user.first_name;
 
-export const getUserSecondName = (state: IUser) => state.second_name;
+export const getUserSecondName = (state: IStateScheme) => state.user.second_name;
 
-export const getUserDisplayName = (state: IUser) => state.display_name;
+export const getUserDisplayName = (state: IStateScheme) => state.user.display_name;
 
-export const getUserPhone = (state: IUser) => state.phone;
+export const getUserPhone = (state: IStateScheme) => state.user.phone;
 
-export const getUserAvatar = (state: IUser) => state.avatar;
+export const getUserAvatar = (state: IStateScheme) => state.user.avatar;
