f0746b6aef83bf755d9f1b0ffd83359728efa282
Tim <80040087+swash1@users.noreply.github.com>
2022-12-13 00:23:05 +0300

    feat(client): tm09-83: Добавить любое web api - fullscreen (#28)

--- packages/client/src/pages/gamePage/GamePage.tsx
+++ packages/client/src/pages/gamePage/GamePage.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Space, Typography } from 'antd';
+import { ShrinkOutlined, ArrowsAltOutlined } from '@ant-design/icons';
 import { TrafficRacer } from './components/TrafficRacer/TrafficRacer';
 import { GameStart } from './components/startGame/GameStart';
 import { GameEnd } from './components/gameEnd/GameEnd';
@@ -8,18 +9,56 @@ import './GamePage.scss';
 
 const { Text, Title } = Typography;
 
+const fullscreenIconClassName = 'game-page__fullscreen-button-icon';
+
 export const GamePage = () => {
   const { height } = useWindowSize();
   const [pageTopOffset, setPageTopOffset] = useState(0);
   const [isGameStarted, setGameStarted] = useState(false);
   const [isGameOver, setGameOver] = useState(false);
   const [score, setScore] = useState(0);
+  const [isFullscreenMode, setFullscreenMode] = useState(false);
   const isFirstStart = !isGameStarted && !isGameOver;
   const gamePageRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (gamePageRef.current) setPageTopOffset(gamePageRef.current.getBoundingClientRect().top);
   }, []);
+
+  const toggleFullscreen = useCallback(() => {
+    const game = gamePageRef.current;
+
+    if (game) {
+      if (isFullscreenMode && document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        game.requestFullscreen();
+      }
+      setFullscreenMode(!isFullscreenMode);
+    }
+  }, [isFullscreenMode]);
+
+  useEffect(() => {
+    const handleExitFullscreen = () => {
+      if (!document.fullscreenElement) {
+        setFullscreenMode(false);
+      }
+    };
+
+    const toggleFullscreenByKeys = (event: KeyboardEvent) => {
+      if (event.shiftKey && event.key === 'F11') {
+        toggleFullscreen();
+      }
+    };
+
+    document.addEventListener('fullscreenchange', handleExitFullscreen);
+    document.addEventListener('keydown', toggleFullscreenByKeys);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleExitFullscreen);
+      document.removeEventListener('keydown', toggleFullscreenByKeys);
+    };
+  }, [toggleFullscreen]);
 
   return (
     <div ref={gamePageRef} className="game-page">
@@ -35,11 +74,19 @@ export const GamePage = () => {
       )}
 
       <TrafficRacer
-        height={height - pageTopOffset}
+        height={isFullscreenMode ? height : height - pageTopOffset}
         setGameStarted={setGameStarted}
         setGameOver={setGameOver}
         setScore={setScore}
       />
+
+      <button type="button" className="game-page__fullscreen-button" onClick={toggleFullscreen}>
+        {isFullscreenMode ? (
+          <ShrinkOutlined className={fullscreenIconClassName} />
+        ) : (
+          <ArrowsAltOutlined className={fullscreenIconClassName} />
+        )}
+      </button>
     </div>
   );
 };
