f1b10d66fa5fe18c5b7e10395f8adf5d61b91392
Al <102159279+Al-nvrsk@users.noreply.github.com>
2022-12-12 09:46:21 +0400

    fix: selectors (redux) (#29)

--- packages/client/src/utils/store/selectors/getUserFieldSelectors/getUserFieldSelectors.test.ts
+++ packages/client/src/utils/store/selectors/getUserFieldSelectors/getUserFieldSelectors.test.ts
@@ -1,5 +1,5 @@
 import { DeepPartial } from '@reduxjs/toolkit';
-import { IUser } from '@/typings/IUser';
+import { IStateScheme } from '@/typings/IStateSchema';
 import {
   getUserAvatar,
   getUserDisplayName,
@@ -13,51 +13,67 @@ import {
 
 describe('getFieldSelectors', () => {
   test('should return user id', () => {
-    const state: DeepPartial<IUser> = {
-      id: 11,
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        id: 11,
+      },
     };
-    expect(getUserId(state as IUser)).toEqual(11);
+    expect(getUserId(state as IStateScheme)).toEqual(11);
   });
   test('should return user Email', () => {
-    const state: DeepPartial<IUser> = {
-      email: 'test@yande.ru',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        email: 'test@yande.ru',
+      },
     };
-    expect(getUserEmail(state as IUser)).toEqual('test@yande.ru');
+    expect(getUserEmail(state as IStateScheme)).toEqual('test@yande.ru');
   });
   test('should return user login', () => {
-    const state: DeepPartial<IUser> = {
-      login: 'Winner',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        login: 'Winner',
+      },
     };
-    expect(getUserLogin(state as IUser)).toEqual('Winner');
+    expect(getUserLogin(state as IStateScheme)).toEqual('Winner');
   });
   test('should return user first name', () => {
-    const state: DeepPartial<IUser> = {
-      first_name: 'Siri',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        first_name: 'Siri',
+      },
     };
-    expect(getUserFirstName(state as IUser)).toEqual('Siri');
+    expect(getUserFirstName(state as IStateScheme)).toEqual('Siri');
   });
   test('should return user second name', () => {
-    const state: DeepPartial<IUser> = {
-      second_name: 'Alisa',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        second_name: 'Alisa',
+      },
     };
-    expect(getUserSecondName(state as IUser)).toEqual('Alisa');
+    expect(getUserSecondName(state as IStateScheme)).toEqual('Alisa');
   });
   test('should return user display name', () => {
-    const state: DeepPartial<IUser> = {
-      display_name: 'Admin',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        display_name: 'Admin',
+      },
     };
-    expect(getUserDisplayName(state as IUser)).toEqual('Admin');
+    expect(getUserDisplayName(state as IStateScheme)).toEqual('Admin');
   });
   test('should return user phone number', () => {
-    const state: DeepPartial<IUser> = {
-      phone: '1234567890',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        phone: '1234567890',
+      },
     };
-    expect(getUserPhone(state as IUser)).toEqual('1234567890');
+    expect(getUserPhone(state as IStateScheme)).toEqual('1234567890');
   });
   test('should return user Avatar path', () => {
-    const state: DeepPartial<IUser> = {
-      avatar: 'path to avatar',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        avatar: 'path to avatar',
+      },
     };
-    expect(getUserAvatar(state as IUser)).toEqual('path to avatar');
+    expect(getUserAvatar(state as IStateScheme)).toEqual('path to avatar');
   });
 });
--- packages/client/src/utils/store/selectors/getUserFullSelector/getUserFullSelector.test.ts
+++ packages/client/src/utils/store/selectors/getUserFullSelector/getUserFullSelector.test.ts
@@ -1,20 +1,22 @@
 import { DeepPartial } from '@reduxjs/toolkit';
-import { IUser } from '@/typings/IUser';
 import { getUserFull } from './getUserFullSelector';
+import { IStateScheme } from '@/typings/IStateSchema';
 
 describe('getUserFull', () => {
   test('should return full user info', () => {
-    const state: DeepPartial<IUser> = {
-      id: 0,
-      email: '1122',
-      login: '3344',
-      first_name: '5566',
-      second_name: '7788',
-      display_name: '9900',
-      phone: '1122',
-      avatar: '3344',
+    const state: DeepPartial<IStateScheme> = {
+      user: {
+        id: 0,
+        email: '1122',
+        login: '3344',
+        first_name: '5566',
+        second_name: '7788',
+        display_name: '9900',
+        phone: '1122',
+        avatar: '3344',
+      },
     };
-    expect(getUserFull(state as IUser)).toEqual({
+    expect(getUserFull(state as IStateScheme)).toEqual({
       id: 0,
       email: '1122',
       login: '3344',
