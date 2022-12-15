56f7387f31110c017e5a3eb54b36327ebf24aa03
Larry Bezrukov <larry9498@gmail.com>
2022-12-15 15:20:04 +0400

    fix: test error

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StoreProvider } from './utils/store/StoreProvider';
 import App from './App';
 
 jest.mock('./utils/router/AppRouter');
@@ -10,8 +11,10 @@ global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey'
 
 test('Example test', async () => {
   render(
-    <Router>
-      <App />
-    </Router>
+    <StoreProvider>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
   );
 });
