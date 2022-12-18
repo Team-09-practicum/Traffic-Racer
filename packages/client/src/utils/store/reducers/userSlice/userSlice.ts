170aff42070998bc8803f2f3439a1b8ba6606f9c
Larry Bezrukov <larry9498@gmail.com>
2022-12-18 15:45:17 +0400

    fix: change initialState, remove unnecessary reducer actions

--- packages/client/src/utils/store/reducers/userSlice/userSlice.ts
+++ packages/client/src/utils/store/reducers/userSlice/userSlice.ts
@@ -2,53 +2,28 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from '@/typings/IUser';
 import { fetchUser } from '../thunks/fetchUserThunk';
 
-const initialState: IUser = {
-  id: 0,
-  email: '',
-  login: '',
-  first_name: '',
-  second_name: '',
-  display_name: '',
-  phone: '',
-  avatar: '',
+interface userState {
+  userInfo: IUser | null;
+}
+
+const initialState: userState = {
+  userInfo: null,
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    changeUserFullInfo: (state, action: PayloadAction<IUser>) => ({ ...action.payload }),
-    changeUserId: (state, action: PayloadAction<number>) => {
-      state.id = action.payload;
-    },
-    changeUserEmail: (state, action: PayloadAction<string>) => {
-      state.email = action.payload;
-    },
-    changeUserLogin: (state, action: PayloadAction<string>) => {
-      state.login = action.payload;
-    },
-    changeUserFirstName: (state, action: PayloadAction<string>) => {
-      state.first_name = action.payload;
-    },
-    changeUserSecondName: (state, action: PayloadAction<string>) => {
-      state.second_name = action.payload;
-    },
-    changeUserDisplayName: (state, action: PayloadAction<string>) => {
-      state.display_name = action.payload;
-    },
-    changeUserPhone: (state, action: PayloadAction<string>) => {
-      state.phone = action.payload;
-    },
-    changeUserAvatar: (state, action: PayloadAction<string>) => {
-      state.avatar = action.payload;
+    changeUserFullInfo: (state, action: PayloadAction<IUser>) => {
+      state.userInfo = action.payload ? action.payload : null;
     },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchUser.fulfilled, (state, action: PayloadAction<IUser>) => {
       if (action.payload) {
-        state = Object.assign(state, action.payload);
+        state.userInfo = action.payload;
       } else {
-        state = Object.assign(state, initialState);
+        state.userInfo = null;
       }
     });
   },
