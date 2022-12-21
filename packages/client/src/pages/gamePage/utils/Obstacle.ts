800fc07c078882b9f4f97304082ffc0232f6a782
Valeria Stepanova <shestakovalera200@mail.ru>
2022-12-21 20:34:35 +0300

    fix: error with oil image

--- packages/client/src/pages/gamePage/utils/Obstacle.ts
+++ packages/client/src/pages/gamePage/utils/Obstacle.ts
@@ -5,6 +5,7 @@ import puddleImg1 from '../assets/obstacle/puddle1.png';
 import puddleImg2 from '../assets/obstacle/puddle2.png';
 import puddleImg3 from '../assets/obstacle/puddle3.png';
 import puddleImg4 from '../assets/obstacle/puddle4.png';
+import oil from '../assets/obstacle/oil.png';
 
 const puddleImgArr = [puddleImg1, puddleImg2, puddleImg3, puddleImg4];
 
@@ -42,7 +43,7 @@ export class Obstacle {
     this.type = type;
     const i = getRandomIntBetweenInterval(0, puddleImgArr.length - 1);
     if (this.type === GameConfig.obstacle.oil) {
-      this.image.src = 'src/pages/gamePage/assets/obstacle/oil.png';
+      this.image.src = oil;
     } else {
       this.image.src = puddleImgArr[i];
     }
