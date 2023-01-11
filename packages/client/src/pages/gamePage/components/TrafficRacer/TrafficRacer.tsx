df82131810ae385c909774288c88a2f1f378e0c1
Valeria Stepanova <shestakovalera200@mail.ru>
2023-01-11 13:51:09 +0300

    feat: add  sound for puddle

--- packages/client/src/pages/gamePage/components/TrafficRacer/TrafficRacer.tsx
+++ packages/client/src/pages/gamePage/components/TrafficRacer/TrafficRacer.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { Dispatch, FC, SetStateAction, useEffect, useRef, memo } from 'react';
 import { useSelector } from 'react-redux';
-import { Scenario, Car, Traffic, GameConfig, crashSound } from '../../utils';
+import { Scenario, Car, Traffic, GameConfig, crashSound, puddleSound } from '../../utils';
 import './TrafficRacer.scss';
 import { getIsSoundOn } from '@/utils/store/selectors/getAppStatusSelectors/getAppStatusSelectors';
 import gameSoundPath from '../../assets/sounds/gameSound.mp3';
@@ -79,6 +79,12 @@ export const TrafficRacer: Props = memo(({ height, setGameStarted, setGameOver, 
     traffic.update(speed);
 
     if (player.current?.passedOnPuddle) {
+      if (!isOver.current) {
+        if (isSoundOn.current) {
+          puddleSound().play();
+        }
+      }
+
       setScore((prev) => prev - GameConfig.obstacle.pointsLossOnPuddle);
       player.current.passedOnPuddle = false;
     }
