e8b0d4caa046fa1e8f3a6a8aa6c0df9d2813637e
Alexander Ershov <rufflet@ya.ru>
2023-01-30 00:37:20 +0300

    fix(client): getServiceId onError handler to support updated typing

--- packages/client/src/controllers/getServiceId.ts
+++ packages/client/src/controllers/getServiceId.ts
@@ -1,9 +1,15 @@
+import { isAxiosError } from 'axios';
 import { api } from '@/utils/api';
 import { showNetworkError } from '@/utils/showNetworkError';
 
 export const getServiceId = async () => {
   const serviceId = await api.getOAuthServiceId({
-    onError: (err) => showNetworkError(err.response.data.reason),
+    onError: (err) => {
+      if (isAxiosError(err) && err.response) {
+        showNetworkError(err.response.data.reason);
+      }
+      throw Error(err.message);
+    },
   });
   return serviceId;
 };
