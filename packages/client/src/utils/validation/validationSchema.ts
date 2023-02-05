978daa7567eace68bce474802059cdb0ced64093
Al <Batuten.mail@gmail.com>
2023-02-05 10:22:26 +0400

    fix: validation schema

--- packages/client/src/utils/validation/validationSchema.ts
+++ packages/client/src/utils/validation/validationSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 import 'yup-phone';
-// import wordsCounter from 'word-counting';
+import wordsCounter from 'word-counting';
 
 export const authSchema = yup.object().shape({
   login: yup
@@ -80,19 +80,21 @@ export const createTopicSchema = yup.object().shape({
     .required('Пожалуйста, введите название темы')
     .min(5, 'Название темы не должно быть короче 5 символов')
     .max(100, 'Название темы не должно превышать 100 символов'),
-  rich_text: yup.string(),
-  // .test(
-  //   'Минимум одно слово',
-  //   'Пожалуйста, введите текст сообщения',
-  //   (value) => wordsCounter(value || '', { isHtml: true }).wordsCount > 0
-  // ),
+  rich_text: yup
+    .string()
+    .test(
+      'Минимум одно слово',
+      'Пожалуйста, введите текст сообщения',
+      (value) => wordsCounter(value || '', { isHtml: true }).wordsCount > 0
+    ),
 });
 
 export const replyInTopicSchema = yup.object().shape({
-  rich_text: yup.string(),
-  // .test(
-  //   'Минимум одно слово',
-  //   'Пожалуйста, введите текст сообщения',
-  //   (value) => wordsCounter(value || '', { isHtml: true }).wordsCount > 0
-  // ),
+  rich_text: yup
+    .string()
+    .test(
+      'Минимум одно слово',
+      'Пожалуйста, введите текст сообщения',
+      (value) => wordsCounter(value || '', { isHtml: true }).wordsCount > 0
+    ),
 });
